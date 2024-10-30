bc694fe273659e1991cf28195fe4a5817e5ee255
kritikaVijaysinghRajput <kritikarajput203@gmail.com>
2024-10-30 14:30:51 +0530

    frontend update

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   server: {
     proxy: {
-      "/api": "http://localhost:3000",
+      "/api": "https://yoliday-backendd.onrender.com",
     },
   },
 });
