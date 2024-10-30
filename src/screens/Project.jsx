9b623055b6042dad467d2d41c59b95c16e41e730
kritikaVijaysinghRajput <kritikarajput203@gmail.com>
2024-10-30 15:48:59 +0530

    some update

--- src/screens/Project.jsx
+++ src/screens/Project.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import Header from "../components/Header";
 import Search from "../components/Search";
 import Card from "../components/Card";
-const projectData = require("./data/data.json");
+import projectData from "../../../server/data/data.json";
 
 function Project() {
   const [activeTab, setActiveTab] = useState("Project");
@@ -13,10 +13,14 @@ function Project() {
   );
 
   return (
-    <div>
-      <Header activeTab={activeTab} setActiveTab={setActiveTab} />
-      <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-      <div className="mt-4 grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-1">
+    <div className="p-4">
+      <p className="mb-2 text-lg font-base">Portfolio</p>
+      <div className="flex flex-col md:flex-row md:justify-between md:items-center gap-4">
+        <Header activeTab={activeTab} setActiveTab={setActiveTab} />
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+
+      <div className="mt-4 space-y-4">
         {filteredProjects.map((project, index) => (
           <Card key={index} project={project} />
         ))}
